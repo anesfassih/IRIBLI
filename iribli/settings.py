05855910427ae5d1148c2f02bd992faa70bf825f
Fassih Anes <ns-fs@hotmail.fr>
2020-12-19 20:49:09 +0100

    ...

--- iribli/settings.py
+++ iribli/settings.py
@@ -11,21 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vvy6^#!t0r$0@0#&o5jaf@*g45hxngesmw)e9^tsmizasm6#a*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["146.59.169.77", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
